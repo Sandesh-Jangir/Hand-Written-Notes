3ee0e8000a948c82f975cb9f8e000c79743322b1
Sandesh Jangir <sandeshatgithub@gmail.com>
2022-07-17 21:02:06 +0530

    Compleated the Desktop View

--- index.js
+++ index.js
@@ -27,12 +27,13 @@ function showNotes() {
     let html = "";
     notesObj.forEach(function (element, index) {
         html += `
-            <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
-                <div class="card-body">
+            <div class="noteCard">
+          
                     <h5 class="card-title">Note ${index + 1}</h5>
                     <p class="card-text">${element}</p>
-                    <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
-                </div>
+                    <div class="card-btm">
+                    <button id="${index}" onclick="deleteNote(this.id)" class="btn-dlt">Delete Note</button>
+                    </div>
             </div>
         `
 
@@ -42,7 +43,7 @@ function showNotes() {
         notesElm.innerHTML = html;
     }
     else{
-        notesElm.innerHTML = `No notes available`
+        notesElm.innerHTML = `No notes available ...`
     }
 }
 
